cd678dda018ef4a3e8cb81ebf411c59015005962
Linus1Kn <linus1knuts@gmail.com>
2026-09-04 10:52:27 +0300

    reactnative test

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,17 +1,23 @@
-import { useState } from 'react'
+import axios from "axios"
+import { useState, useEffect } from 'react'
 import PersonsForm from './components/PersonsForm'
 import PersonsList from './components/PersonsList'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [nameFilter, setNameFilter] = useState('')
+
+  useEffect(() => {
+    const eventHandler = response => {
+      console.log('promise fulfilled')
+      setPersons(response.data)
+    }
+
+    const promise = axios.get('http://localhost:3001/persons')
+    promise.then(eventHandler)
+  }, [])
 
   const filteredPersons = persons.filter(person =>
     person.name.toLowerCase().includes(nameFilter.toLowerCase())
